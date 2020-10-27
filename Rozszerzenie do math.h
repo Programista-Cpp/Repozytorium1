8baffe7fabc02ce016f43948ae483b7b1728298d
Programista-Cpp <70840444+Programista-Cpp@users.noreply.github.com>
2020-10-27 17:34:45 +0100

    Dodanie funkcji Signum(x), PoleKwadratu(a), PoleProstokata(a, b).

--- Rozszerzenie do math.h
+++ Rozszerzenie do math.h
@@ -3,5 +3,37 @@
 
 using namespace std;
 
-int main()
+namespace funkcjeMatematyczne
 {
+  int Signum(auto x)
+  {
+    if(static_cast<long long>(x) != x || static_cast<long double>(x) != x || static_cast<unsigned long long>(x) != x)
+    {
+      cerr<<"Podaj numer!"<<endl;
+      return Signum(x);
+    else
+    {
+      if(x > 0) return 1;
+      else if(x < 0) return -1;
+      else return 0;
+    }
+  }
+  auto PoleKwadratu(auto a)
+  {
+    if(static_cast<long long>(a) != a || static_cast<long double>(a) != a || static_cast<unsigned long long>(a) != a)
+    {
+      cerr<<"Podaj numer!"<<endl;
+      return PoleKwadratu(a);
+    }
+    else return pow(a,2);
+  auto PoleProstokata(auto a, auto b)
+  {
+    if(static_cast<long long>(a) != a || static_cast<long double>(a) != a || static_cast<unsigned long long>(a) != a ||
+       static_cast<long long>(b) != b || static_cast<long double>(b) != b || static_cast<unsigned long long>(b) != b)
+    {
+      cerr<<"Podaj numer/numery!"<<endl;
+      return PoleProstokata(a, b);
+    }
+    else return a*b;
+  }
+}
