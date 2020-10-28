f4db55a681dbe85df9f1b67f86b0659c859c2479
Programista-Cpp <70840444+Programista-Cpp@users.noreply.github.com>
2020-10-28 15:51:27 +0100

    Zmienienie z polskiego na angielski

--- Rozszerzenie do math.h
+++ Rozszerzenie do math.h
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-namespace funkcjeMatematyczne
+namespace mathFunctions
 {
   int Signum(auto x)
   {
     if(static_cast<long long>(x) != x || static_cast<long double>(x) != x || static_cast<unsigned long long>(x) != x)
     {
-      cerr<<"Podaj numer!"<<endl;
+      cerr<<"Podaj numer! Give number!"<<endl;
       return Signum(x);
     } //Wyjatek
     else
@@ -19,21 +19,21 @@ namespace funkcjeMatematyczne
       else return 0;
     }
   }
-  auto PoleKwadratu(auto a)
+  auto SquareArea(auto a)
   {
     if(static_cast<long long>(a) != a || static_cast<long double>(a) != a || static_cast<unsigned long long>(a) != a)
     {
-      cerr<<"Podaj numer!"<<endl;
-      return PoleKwadratu(a);
+      cerr<<"Podaj numer! Give number!"<<endl;
+      return SquareArea(a);
     } //Wyjatek
     else return pow(a,2); //From math.h
-  auto PoleProstokata(auto a, auto b)
+  auto RectangleArea(auto a, auto b)
   {
     if(static_cast<long long>(a) != a || static_cast<long double>(a) != a || static_cast<unsigned long long>(a) != a ||
        static_cast<long long>(b) != b || static_cast<long double>(b) != b || static_cast<unsigned long long>(b) != b)
     {
-      cerr<<"Podaj numer/numery!"<<endl;
-      return PoleProstokata(a, b);
+      cerr<<"Podaj numer/numery! Give nmber/numbers!"<<endl;
+      return RectangleArea(a, b);
     } //Wyjatek
     else return a*b;
   }
