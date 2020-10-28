c02cdbbf33a7d061821cf30c3dc2d9282914a170
Programista-Cpp <70840444+Programista-Cpp@users.noreply.github.com>
2020-10-28 15:35:56 +0100

    Dodane komentarze.

--- Tlo_gry(Definicje)_gra_tekstowa.hpp
+++ Tlo_gry(Definicje)_gra_tekstowa.hpp
@@ -3,34 +3,34 @@
 using namespace std;
 struct Gracz
 {
-	short Sila;
-	short Zycie;
-	string Imie;
-	string Nazwisko;
-	enum Bron {MIECZ, LUK, STRZALY, KUSZA, NOZ, TOPOR, null};
-	enum Slabosci { enum Strachprzed { CIEMNOSC, PAJAKI, SZCZURY, PSZCZOLY, STRACH, null }, SLODYCZE, NIEPELNOSPRAWNOSC, null };
-	auto Atakuj(Bron bron, Przeciwnik Wrog)
+	short  Sila;															//Sila     Postaci Gracza
+	short  Zycie;															//Zycie    Postaci Gracza
+	string Imie;															//Imie     Postaci Gracza
+	string Nazwisko;														//Nazwisko Postaci Gracza
+	enum   Bron {MIECZ, LUK, STRZALY, KUSZA, NOZ, TOPOR, null};									//Bron     Postaci Gracza
+	enum   Slabosci { enum Strachprzed { CIEMNOSC, PAJAKI, SZCZURY, PSZCZOLY, STRACH, null }, SLODYCZE, NIEPELNOSPRAWNOSC, null };	//Slabosci Postaci Gracza
+	auto   Atakuj(Bron bron, Przeciwnik Wrog)
 	{
-		int wybor=0;
-		cout << "Czym chcesz atakowac? (1-miecz, 2-luk, 3-kusza,4-topor) : " << endl;
-		if(        bron == MIECZ && GraczMaTakaBron(MieczGracza))
+		//int wybor=0;
+		cout << "Czym chcesz atakowac? (1-miecz, 2-luk, 3-kusza,4-topor) : " << endl;						//Pytanie o bron
+		if(     bron == MIECZ && GraczMaTakaBron(MieczGracza))									//Sprawdzenie czy gracz ma miecz
 			Wrog.Zycie -= (bron.Sila + Gracz.Sila - Wrog.Pancerz);
-		else if(bron == MIECZ && !GraczMaTakaBron(MieczGracza)) cerr<<"Nie masz miecza!"<<endl;
+		else if(bron == MIECZ && !GraczMaTakaBron(MieczGracza)) cerr<<"Nie masz miecza!"<<endl;					//Err jesli nie ma
 
-		else if(bron == LUK     &&  GraczMaTakaBron(LukGracza))
+		else if(bron == LUK   &&  GraczMaTakaBron(LukGracza))									//Sprawdzenie czy gracz ma luk
 			Wrog.Zycie -= (bron.Sila + Gracz.Sila - Wrog.Pancerz);
-		else if(bron == LUK     && !GraczMaTakabron(LukGracza))      cerr<<"Nie masz luku!"<<endl;
+		else if(bron == LUK   && !GraczMaTakabron(LukGracza))      cerr<<"Nie masz luku!"<<endl;				//Err jesli nie ma
 
-		else if(bron == KUSZA&& GraczMaTakaBron(KuszaGracza))
+		else if(bron == KUSZA && GraczMaTakaBron(KuszaGracza))									//Sprawdzenie czy grac ma kusze
 			Wrog.Zycie -= (bron.Sila + Gracz.Sila - Wrog.Pancerz);
-		else if(bron == KUSZA&& !GraczMaTakaBron(KuszaGracza))  cerr<<"Nie masz kuszy!"<<endl;
+		else if(bron == KUSZA && !GraczMaTakaBron(KuszaGracza))  cerr<<"Nie masz kuszy!"<<endl;					//Err jesli nie ma
 
-		else if(bron == TOPOR&& GraczMaTakaBron(ToporGracza))
+		else if(bron == TOPOR && GraczMaTakaBron(ToporGracza))									//Sprawdzenie czy gracz ma topor
 			Wrog.Zycie -= (bron.Sila + Gracz.Sila - Wrog.Pancerz);
-		else if(bron == TOPOR&& !GraczMaTakaBron(ToporGracza)) cerr<<"Nie masz toporu!"<<endl;
+		else if(bron == TOPOR && !GraczMaTakaBron(ToporGracza)) cerr<<"Nie masz toporu!"<<endl;					//Err jesli nie ma
 		return Wrog.Zycie;
 	}
-	auto BronSie(Przeciwnik.Bron bron_wroga, Przeciwnik Wrog)
+	auto   BronSie(Przeciwnik.Bron bron_wroga, Przeciwnik Wrog)
 	{
 		Gracz.Zycie -= (bron_wroga.Sila + Wrog.Sila - (LosujObrone() + PancerzGracza));
 		return Gracz.Zycie;
