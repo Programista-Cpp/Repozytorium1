895027b95559a74cb2b29d336caefe8f66371150
Programista-Cpp <70840444+Programista-Cpp@users.noreply.github.com>
2020-11-12 10:23:28 +0100

    Dodana struktura Physics::quantMechs::quantObj.
    Added structure Physics::quantMechs::quantObj.

--- Rozszerzenie do math.h
+++ Rozszerzenie do math.h
@@ -20,5 +20,24 @@
   
     auto ParallelogramArea(auto a, auto h)
       return RectangleArea(a, h);
-  } //namespace mf
+  } // namespace mf
+  namespace Pphysics
+  {
+    namespace quantMechs
+    {
+      static unsigned int VoidQuantState; // |0>
+      struct quantObj
+      {
+        unsigned int m; // mass
+        unsigned int QuantState; // quantum state, |n>
+        unsigned int Creation(quantObj.QuantState&)
+          return ++quantObj.QuantState; /// If you want to change quantum sate of void to |2>, you must |0> * (creation operator)^2 (in this func: Creation(Creation(VoidQuantState));
+        
+        unsigned int Anihilation(quantObj.QuantState&) return --quantObj.QuantState;
+        std::ostream& operator<<(std::ostream& quant, quantObj.QuantState) // return |n>
+          return std::cout << '|' << quantObj.QuantState << '>';
+      }
+      unsigned int VoidQuantState = 0;
+    } // namespace quantObj
+  } // namespace Physics
 #endif
