c0f69d9c5db01529dbc9014dbb706afc39d60c18
Programista-Cpp <70840444+Programista-Cpp@users.noreply.github.com>
2020-11-26 19:32:20 +0100

    Added pluralTheorem, Range, Clamp( ), Lerp( ), SafeDiv( ), DefaultSafeDiv( ).

--- ext_math.hpp
+++ ext_math.hpp
@@ -1,6 +1,7 @@
 #ifndefef EXT_MATH_H_DOLACZONE
-  #define EXT_MATH_H_DOLACZONE
-  
+  #define EXT_MATH_H_DOLACZONE 1
+  #define DEFAULT_ERR -1
+  #define ERR(x) std::cerr << x'
   // Math
   namespace Math /// This is an incomplete namespace, do not download this. Version: 0.3
   {
@@ -62,6 +63,23 @@
          }
        }
     }
+    namespace proto_pluralTheorem
+    {
+        struct Range
+        {
+            enum RANGE {OPEN, CLOSE} range;
+            bool IsOpen() const
+            {
+                return (range == OPEN);
+            }
+            bool IsClose() const
+            {
+                return (range == CLOSE);
+            }
+            auto min;
+            auto max;
+        };
+    } // namespace pluralTheorem
     namespace beta_mathFuncs /// This is an incomplete namespace, do not download this. Version: 0.5
     {
         int Signum(auto x)
@@ -90,7 +108,35 @@
         {
             return RectangleArea(a, h);
         }
+        auto DefaultSafeDiv(auto a, auto b)
+        {
+            return b != 0 ? a / b : DEFAULT_ERR;
+        }
+        auto SafeDiv(auto a, auto b, auto  err_arg)
+        {
+            return (b != 0) ? ((a / b) : ERR(err_arg)));
+        }
+        auto InterpolateFunc(auto y0, auto y1, auto x0, auto x, auto x1) // "Lerp"; Returns y
+        {
+            return (y0 + ( ( (y1 - y0) / (x1 - x0) ) * (x - x0) ) );
+        }
+        
+        auto Clamp(auto val, Range range);
+        {
+            if(range.IsOpen()
+            {
+                if((range.min < val) && (val < range.max)) return val;
+                else if(range.min >= val) { val = range.min; return val; }
+                else { val = range.max; return val; }
+            else if(range.IsClose())
+            {
+                if((range.min <= val) && (val <= range.max)) return val;
+                else if(range.min > val) { val = range.min; return val; }
+                else { val = range.max; return val; }
+            }
+        }
     } // namespace mathFuncs
+    
   } // namespace Math
   
   //=======================================================================================
